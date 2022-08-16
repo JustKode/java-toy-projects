ee4de154c5b95c6a1348ea148694be0eb6f6ef49
justkode <sobu0715@gmail.com>
2022-08-17 00:26:21 +0900

    feat: add Counter example

--- prometheus-grafana-example/src/main/java/PrometheusApplication.java
+++ prometheus-grafana-example/src/main/java/PrometheusApplication.java
@@ -1,5 +1,7 @@
+import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.Histogram;
+import io.prometheus.client.Summary;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.slf4j.Logger;
@@ -8,6 +10,12 @@ import org.slf4j.LoggerFactory;
 import java.util.Random;
 
 public class PrometheusApplication {
+    static private final Counter counter = Counter.build()
+            .name("request_counter")
+            .labelNames("request")
+            .help("요청 횟수")
+            .register();
+
     static private final Gauge gauge = Gauge.build()
             .name("accuracy_percent")
             .labelNames("accuracy")
@@ -15,7 +23,7 @@ public class PrometheusApplication {
             .register();
 
     static private final Histogram histogram = Histogram.build()
-            .name("requests")
+            .name("requests_time")
             .labelNames("request")
             .buckets(0.1, 0.3, 0.5, 1.0)
             .help("요청")
@@ -32,6 +40,7 @@ public class PrometheusApplication {
                 Random random = new Random();
                 Histogram.Timer startTimer = histogram.labels("request").startTimer();
                 Thread.sleep(random.nextInt(1000));
+                counter.labels("request").inc(2.0);
                 startTimer.observeDuration();
                 gauge.labels("accuracy").set(random.nextInt(20) + 81);
             }
